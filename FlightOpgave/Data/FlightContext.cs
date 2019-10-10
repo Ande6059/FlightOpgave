6dc6986de08a71bfd5e335df0bf4dfec057b5b1c
Anders Weiskvist <Weiskvist94@gmail.com>
2019-10-10 13:24:48 +0200

    Add project files.

---
+++ FlightOpgave/Data/FlightContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightOpgave.Models
+{
+    public class FlightContext : DbContext
+    {
+        public FlightContext (DbContextOptions<FlightContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FlightOpgave.Models.Flight> Flight { get; set; }
+    }
+}
---
+++ FlightOpgave/Models/Flight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlightOpgave.Models
+{
+    public class Flight
+    {
+        public int FlightId { get; set; }
+
+        [Display(Name = "Aircraft Type")]
+        public string AircraftType { get; set; }
+
+        [Display(Name = "From Location")]
+        public string FromLocation { get; set; }
+
+        [Display(Name = "To Location")]
+        public string ToLocation { get; set; }
+
+        [Display(Name = "Departure Time")]
+        public DateTime DepartureTime { get; set; }
+
+        [Display(Name = "Arrival Time")]
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}
+
